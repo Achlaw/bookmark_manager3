0a1eb794a9edcdb8e1a1527aca8e53d8490cd9e4
Alex Law <achlawuk@gmail.com>
2017-05-04 13:42:09 +0100

    Moved datamapper to app.rb from links.rb

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,12 @@
 ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require_relative 'models/link.rb'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
 
