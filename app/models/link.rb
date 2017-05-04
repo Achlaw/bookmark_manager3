be88d913e4a25a14288e61d99024ae3ab5bff451
Alex Law <achlawuk@gmail.com>
2017-05-04 12:15:39 +0100

    Redirect / to links

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,7 @@ class Link
   # property :tag, String
 end
 
+DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
